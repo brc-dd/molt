0cce99ff3684f4973286ec276b439b60da56d37e
hasundue <hasundue@gmail.com>
2024-06-20 10:13:46 +0900

    reimplementing core/lockfile

--- core/lockfile.ts
+++ core/lockfile.ts
@@ -1,382 +1,41 @@
-import { ensure, is } from "@core/unknownutil";
+import { omit } from "@std/collections";
 import { toPath } from "@molt/lib/path";
-import { assertEquals } from "@std/assert";
 import {
-  deepMerge,
-  distinctBy,
-  filterEntries,
-  mapNotNullish,
-  omit,
-  partition,
-} from "@std/collections";
-import { detect as detectEOL, EOL } from "@std/fs/eol";
-import { parse, type UpdatedDependency } from "./dependency.ts";
-import type { FileUpdate } from "./file.ts";
-import type { DependencyUpdate } from "./update.ts";
+  type Lockfile,
+  type LockfileJson,
+  parseFromJson,
+} from "./deno_lockfile/js/mod.ts";
 
-// We can't use unknowntuil's `PredicateType` because it results in a
-// 'slow type' for `deno publish`, unfortunately.
-export interface LockFileJson {
-  version: string;
-  packages?: {
-    specifiers: Record<string, string>;
-    jsr?: Record<string, JsrEntry>;
-    npm?: Record<string, NpmEntry>;
-  };
-  remote?: Record<string, string>;
-  workspace?: {
-    dependencies?: string[];
-  };
-}
-
-interface JsrEntry {
-  integrity: string;
-  dependencies?: string[];
-}
-
-interface NpmEntry {
-  integrity: string;
-  dependencies: Record<string, string>;
-}
-
-const isLockFileJson = is.ObjectOf({
-  version: is.String,
-  packages: is.OptionalOf(is.ObjectOf({
-    specifiers: is.RecordOf(is.String, is.String),
-    jsr: is.OptionalOf(is.RecordOf(
-      is.ObjectOf({
-        integrity: is.String,
-        dependencies: is.OptionalOf(is.ArrayOf(is.String)),
-      }),
-      is.String,
-    )),
-    npm: is.OptionalOf(is.RecordOf(
-      is.ObjectOf({
-        integrity: is.String,
-        dependencies: is.RecordOf(is.String, is.String),
-      }),
-      is.String,
-    )),
-  })),
-  remote: is.OptionalOf(is.RecordOf(is.String, is.String)),
-  workspace: is.OptionalOf(is.ObjectOf({
-    dependencies: is.OptionalOf(is.ArrayOf(is.String)),
-  })),
-});
+export type { Lockfile, LockfileJson };
 
 /**
- * A parsed lockfile JSON object.
- * @example
- * ```ts
- * {
- *   version: "3",
- *   packages: {
- *     specifiers: {
- *       "jsr:@core/match@0.1.x": "jsr:@core/match@0.1.9",
- *       "npm:node-emoji@^2": "npm:node-emoji@2.1.3",
- *       "npm:ts-toolbelt@9.6.0": "npm:ts-toolbelt@9.6.0"
- *     },
- *   },
- * }
- */
-
-/** An object representing a lockfile. */
-export interface LockFile {
-  /** The path to the lockfile. */
-  path: string;
-  /** The parsed lockfile JSON object. */
-  data: LockFileJson;
-}
-
-/** A partial lock for a specific dependency */
-export interface LockPart {
-  /** The import specifier of the dependency. */
-  specifier: string;
-  /** The parsed lockfile JSON object. */
-  data: LockFileJson;
-}
-
-/**
- * Read, parse, and validate a lockfile.
- *
- * @param specifier - The URL or path to the lockfile.
- * @returns The parsed JSON object of the lockfile.
- */
-export function parseLockFileJson(
-  content: string,
-): LockFileJson {
-  try {
-    return ensure(JSON.parse(content), isLockFileJson);
-  } catch (cause) {
-    throw new Error(`Failed to parse lockfile`, { cause });
-  }
-}
-
-/**
- * Read, parse, and validate a lockfile.
+ * Create a LockFile object from the given lock file.
  *
  * @param specifier - The URL or path to the lockfile.
  * @returns The parsed `LockFile` object.
  */
 export async function readLockFile(
   specifier: URL | string,
-): Promise<LockFile> {
-  return {
-    path: toPath(specifier),
-    data: parseLockFileJson(await Deno.readTextFile(specifier)),
-  };
-}
-
-//
-// Temporary file management
-//
-interface TempFile {
-  path: string;
-  [Symbol.asyncDispose](): Promise<void>;
-}
-
-async function createTempFile(): Promise<TempFile> {
-  const path = await Deno.makeTempFile();
-  return {
-    [Symbol.asyncDispose]() {
-      return Deno.remove(path);
-    },
-    path,
-  };
-}
-
-export class CommandError extends Error {}
-
-/**
- * Create a partial lockfile for the given dependency as a temporary file and returns
- * the parsed LockFile object.
- *
- * The implementation here is quite inefficient. We should rather add a JS interface to
- * the `deno_lockfile` crate.
- *
- * @param dependency - The import specifier of dependency to create a lockfile for.
- * @param locked - If given, the resulting lock has the same version as this.
- * @param lockTo - If given, the resulting lock has the same version as this.
- * @returns A promise to the updated lockfile.
- */
-export async function createLockPart(
-  dependency: string,
-  locked?: LockFile | null,
-  lockTo?: string,
-): Promise<LockPart> {
-  // Create a dummy module that only includes the given dependencies.
-  const specifier = lockTo ?? locked?.data.packages?.specifiers[dependency] ??
-    dependency;
-  await using mod = await createTempFile();
-  await Deno.writeTextFile(mod.path, `import "${specifier}";\n`);
-
-  // Create a lockfile for the dummy module.
-  await using lock = await createTempFile();
-  const { code, stderr } = await new Deno.Command("deno", {
-    args: [
-      "cache",
-      "--no-config",
-      "--lock-write",
-      "--lock",
-      lock.path,
-      mod.path,
-    ],
-  }).output();
-  if (code !== 0) {
-    throw new CommandError(new TextDecoder().decode(stderr));
-  }
-  const data = parseLockFileJson(await Deno.readTextFile(lock.path));
-  if (locked) {
-    data.packages!.specifiers[dependency] =
-      data.packages!.specifiers[specifier];
-    delete data.packages!.specifiers[specifier];
-  }
-  return {
-    specifier: dependency,
-    data,
-  };
+): Promise<Lockfile> {
+  const path = toPath(specifier);
+  return parseFromJson(path, await Deno.readTextFile(path));
 }
 
 /**
- * Create a new lockfile for each dependency and returns a list of them.
+ * Extract a partial lock for a dependency from a lockfile.
  *
- * @param lockfile - The path to the lockfile.
- * @returns A Promise for the LockFile objects of updated lockfiles.
+ * @param specifier - The import specifier of the JSR package.
+ * @param lockfile - The lockfile to extract a partial lock for the dependency from.
+ * @returns The partial lock file for the dependency.
  */
-export async function createLockPartForEach(
-  lockfile: LockFile,
-  update = true,
-): Promise<LockPart[]> {
-  return await Promise.all(
-    Object.entries(lockfile.data.packages?.specifiers ?? {}).map(
-      ([specifier, locked]): Promise<LockPart> =>
-        createLockPart(update ? specifier : locked),
-    ),
-  );
-}
-
-/**
- * Extract a partial lock file for a dependency from a lock file.
- *
- * @param dependency - The import specifier of the dependency.
- * @param lock - The lockfile to create a partial lock from.
- */
-export async function extractLockPart(
-  dependency: string,
-  lockfile: LockFile,
-): Promise<LockPart> {
-  const specifier = lockfile.data.packages?.specifiers[dependency];
-  if (!specifier) {
-    throw new Error(`${dependency} not found in ${lockfile.path}`);
-  }
-  const lock = await createLockPart(specifier);
-  console.log(lock);
-
-  const deps = Object.keys(lock.data.packages?.specifiers ?? {})
-    .filter((it) => !it.startsWith(specifier));
-  console.log(deps);
-
-  for (const dep of deps) {
-    const lock = await extractLockPart(dep, lockfile);
-    console.log(lock);
-  }
-
-  return lock;
-}
-
-/**
- * Collect updates to dependencies in the given lockfile.
- *
- * @param original - The LockFile object for the original lockfile.
- * @param targets - The specifiers of dependencies being updated.
- * @returns The collected updates to dependencies.
- */
-export async function collectUpdateFromLockFile(
-  original: LockFile,
-  ...targets: string[]
-): Promise<DependencyUpdate<"lockfile">[]> {
-  const patches = targets.length
-    ? await Promise.all(targets.map((it) => createLockPart(it)))
-    : await createLockPartForEach(original);
-  const updates: DependencyUpdate<"lockfile">[] = patches.flatMap((patch) =>
-    mapNotNullish(
-      Object.entries(patch.data.packages?.specifiers ?? {}),
-      ([specifier, locking]) => {
-        const locked = original.data.packages?.specifiers[specifier];
-        if (locked !== locking) {
-          return {
-            from: locked ? parse(locked) : undefined,
-            to: parse(locking) as UpdatedDependency,
-            code: {
-              // TODO: Is it justified to use the specifier of the patch here?
-              specifier: patch.specifier,
-              span: undefined,
-            },
-            // TODO: Is it justified to use the whole patch here?
-            lock: patch.data,
-            map: undefined,
-            referrer: original.path,
-          };
-        }
-      },
-    )
-  );
-  return distinctBy(updates, (update) => update.to.name)
-    .sort((a, b) => a.to.name.localeCompare(b.to.name));
-}
-
-/** Write the given lockfile update to the lockfile. */
-export async function writeToLockfile(
-  update: FileUpdate<"lockfile">,
-) {
-  const content = await Deno.readTextFile(update.path);
-  const original = parseLockFileJson(content);
-
-  for (const dependency of update.dependencies) {
-    const specifier = dependency.code.specifier;
-    console.log(specifier);
-
-    // An updated partial lockfile for the dependency.
-    const patch = dependency.lock;
-    console.log(patch);
-
-    /** Specifiers that are only depended by the current dependency. */
-    const omitter = createLockFileOmitKeys(specifier, update.locks);
-    console.log(omitter);
-
-    if (original.packages && patch.packages) {
-      original.packages.specifiers = deepMerge(
-        original.packages.specifiers,
-        patch.packages.specifiers,
-      );
-      if (patch.packages.jsr) {
-        original.packages.jsr = deepMerge(
-          omit(original.packages.jsr ?? {}, omitter.jsr),
-          patch.packages.jsr,
-          { arrays: "replace" },
-        );
-      }
-      if (patch.packages.npm) {
-        original.packages.npm = deepMerge(
-          omit(original.packages.npm ?? {}, omitter.npm),
-          patch.packages.npm,
-        );
-      }
-    }
-    if (patch.remote) {
-      original.remote = deepMerge(
-        omit(original.remote ?? {}, omitter.remote),
-        patch.remote,
-      );
-    }
-  }
-  await Deno.writeTextFile(
-    update.path,
-    JSON.stringify(original, replacer, 2) + (detectEOL(content) ?? EOL),
-  );
-}
-
-function replacer(
-  key: string,
-  value: unknown,
-) {
-  return ["specifiers", "jsr", "npm", "remote"].includes(key) && value
-    ? Object.fromEntries(Object.entries(value).sort())
-    : value;
-}
-
-interface LockFileOmitKeys {
-  jsr: string[];
-  npm: string[];
-  remote: string[];
-}
-
-/** Create a list of keys to omit from the original lockfile. */
-function createLockFileOmitKeys(
+export async function extractJsrPackage(
   specifier: string,
-  locks: LockPart[],
-): LockFileOmitKeys {
-  const [relevant, others] = partition(
-    locks,
-    (it) => it.specifier === specifier,
+  lockfile: Lockfile,
+): Promise<Lockfile> {
+  const copy = lockfile.copy();
+  copy.setWorkspaceConfig({ dependencies: [specifier] });
+  return await parseFromJson(
+    copy.filename,
+    omit(copy.toJson(), ["remote", "redirects"]),
   );
-  assertEquals(relevant.length, 1, specifier);
-  const { data: patch } = relevant[0];
-  return {
-    jsr: Object.keys(patch.packages?.jsr ?? {}).filter((key) =>
-      !others.some((part) =>
-        Object.keys(part.data.packages?.jsr ?? {}).some((it) => it === key)
-      )
-    ),
-    npm: Object.keys(patch.packages?.npm ?? {}).filter((key) =>
-      !others.some((part) =>
-        Object.keys(part.data.packages?.npm ?? {}).some((it) => it === key)
-      )
-    ),
-    remote: Object.keys(patch.remote ?? {}).filter((key) =>
-      !others.some((part) =>
-        Object.keys(part.data.remote ?? {}).some((it) => it === key)
-      )
-    ),
-  };
 }
--- core/lockfile_test.ts
+++ core/lockfile_test.ts
@@ -1,47 +1,21 @@
-import * as fs from "@chiezo/amber/fs";
-import { assertEquals, assertObjectMatch, assertRejects } from "@std/assert";
-import { describe, it } from "@std/testing/bdd";
-import {
-  collectUpdateFromLockFile,
-  createLockPart,
-  createLockPartForEach,
-  extractLockPart,
-  parseLockFileJson,
-  readLockFile,
-  writeToLockfile,
-} from "./lockfile.ts";
-import { associateByFile, type FileUpdate } from "./file.ts";
-import { collect } from "./update.ts";
+import { assertEquals, assertObjectMatch } from "@std/assert";
+import { beforeEach, describe, it } from "@std/testing/bdd";
+import { extractJsrPackage, type Lockfile, readLockFile } from "./lockfile.ts";
 
-Deno.test("parseLockFileJson", async () =>
-  assertObjectMatch(
-    parseLockFileJson(
-      await Deno.readTextFile(
-        new URL("../test/fixtures/lockfile/deno.updated.lock", import.meta.url),
-      ),
-    ),
-    {
-      version: "3",
-      packages: {
-        specifiers: {
-          "jsr:@core/match@0.1.x": "jsr:@core/match@0.1.9",
-          "npm:hono@^3": "npm:hono@3.12.12",
-          "npm:ts-toolbelt@9.6.0": "npm:ts-toolbelt@9.6.0",
-        },
-      },
-    },
-  ));
-
-describe("createLockPart", () => {
-  it("should create an updated partial lock for a package", async () => {
-    const lock = await createLockPart("jsr:@core/match@0.1.x");
+describe("readLockFile", () => {
+  it("should read a lockfile", async () => {
+    const lockfile = await readLockFile(
+      new URL("../test/fixtures/deno.lock", import.meta.url),
+    );
     assertObjectMatch(
-      lock.data,
+      lockfile.toJson(),
       {
+        version: "3",
         packages: {
           specifiers: {
-            "jsr:@core/match@0.1.x": "jsr:@core/match@0.1.9",
-            "npm:ts-toolbelt@9.6.0": "npm:ts-toolbelt@9.6.0",
+            "jsr:@std/assert@^0.222.0": "jsr:@std/assert@0.222.0",
+            "jsr:@std/fmt@^0.222.0": "jsr:@std/fmt@0.222.0",
+            "jsr:@std/testing@^0.222.0": "jsr:@std/testing@0.222.0",
           },
         },
       },
@@ -49,364 +23,62 @@ describe("createLockPart", () => {
   });
 });
 
-describe.only("extractLockPart", () => {
-  it("should extract a partial lock for a package from a lock file", async () => {
-    const lock = await extractLockPart(
-      "jsr:@std/assert@^0.222.0",
-      await readLockFile(
-        new URL("../test/fixtures/deno.lock", import.meta.url),
-      ),
-    );
-    //assertObjectMatch(lock.data, {
-    //  packages: {
-    //    specifiers: {
-    //      "jsr:@std/assert@^0.222.0": "jsr:@std/assert@0.222.0",
-    //      "jsr:@std/fmt@^0.222.0": "jsr:@std/fmt@0.222.0",
-    //    },
-    //    jsr: {
-    //      "@std/assert@0.222.0": {
-    //        "integrity":
-    //          "cbf00c0d8125a56c087e3d1ea0e638760d47206b30e9d300bad826b811719fc7",
-    //        "dependencies": [
-    //          "jsr:@std/fmt@^0.222.0",
-    //        ],
-    //      },
-    //      "@std/fmt@0.222.0": {
-    //        "integrity": "0eb99babf1cc697d67e76e8753916c037bbc3ce4abcefa321e1465708b0adda1"
-    //      },
-    //    },
-    //  },
-    //});
-  });
+describe("extractJsrPackage", () => {
+  let lockfile: Lockfile;
 
-  it("should extract a fixed partial lock for an entrypoint from a lock file", async () => {
-    const lock = await extractLockPart(
-      "jsr:@std/testing@^0.222.0/bdd",
-      await readLockFile(
-        new URL("../test/fixtures/deno.lock", import.meta.url),
-      ),
+  beforeEach(async () => {
+    lockfile = await readLockFile(
+      new URL("../test/fixtures/deno.lock", import.meta.url),
     );
   });
-});
 
-Deno.test("createLockPart - jsr:@std/testing/bdd", async () => {
-  const lock = await createLockPart("jsr:@std/testing@^0.222.0/bdd");
-  assertEquals(lock.specifier, "jsr:@std/testing@^0.222.0/bdd");
-  assertObjectMatch(
-    lock.data,
-    {
-      packages: {
-        specifiers: {
-          "jsr:@std/testing@^0.222.0": "jsr:@std/testing@0.222.1",
-        },
-      },
-    },
-  );
-});
-
-Deno.test("createLockPart - jsr:@std/testing/bdd - locked", async () => {
-  const lock = await createLockPart(
-    "jsr:@std/testing@^0.222.0/bdd",
-    await readLockFile(new URL("../test/fixtures/deno.lock", import.meta.url)),
-  );
-  assertEquals(lock.specifier, "jsr:@std/testing@^0.222.0/bdd");
-  assertObjectMatch(
-    lock.data,
-    {
+  it("should extract the partial lock for a JSR package from a lockfile", async () => {
+    const part = await extractJsrPackage("jsr:@std/testing@^0.222.0", lockfile);
+    assertEquals(part.toJson(), {
+      version: "3",
       packages: {
         specifiers: {
           "jsr:@std/testing@^0.222.0": "jsr:@std/testing@0.222.0",
         },
-      },
-    },
-  );
-});
-
-Deno.test("createLockPart - jsr:@std/testing", async () => {
-  await assertRejects(() => createLockPart("jsr:@std/testing@^0.222.0"));
-});
-
-Deno.test("createLockPart - npm:hono", async () => {
-  const lock = await createLockPart("npm:hono@^3");
-  assertObjectMatch(
-    lock.data,
-    {
-      packages: {
-        specifiers: {
-          "npm:hono@^3": "npm:hono@3.12.12",
-        },
-      },
-    },
-  );
-});
-
-Deno.test("createLockPart - http", async () => {
-  const lock = await createLockPart(
-    "https://deno.land/x/deno_graph@0.50.0/mod.ts",
-  );
-  assertObjectMatch(
-    lock.data,
-    {
-      remote: {
-        "https://deno.land/x/deno_graph@0.50.0/deno_graph_wasm.generated.js":
-          "e1d58f79f4e33c7cc1062af565600f422a2fec1b5eaee42691f2a7992d9d5e6b",
-        "https://deno.land/x/deno_graph@0.50.0/loader.ts":
-          "a2e757383908f4a51659fe1b1203386887ebb17756bac930a64856d613d8d57d",
-        "https://deno.land/x/deno_graph@0.50.0/media_type.ts":
-          "a89a1b38d07c160e896de9ceb99285ba8391940140558304171066b5c3ef7609",
-        "https://deno.land/x/deno_graph@0.50.0/mod.ts":
-          "47b5e8560f3e66468194742239fc76cf587d611dd43c1913eeebb9b1d94fc39f",
-        "https://deno.land/x/dir@1.5.1/data_local_dir/mod.ts":
-          "91eb1c4bfadfbeda30171007bac6d85aadacd43224a5ed721bbe56bc64e9eb66",
-        "https://deno.land/x/wasmbuild@0.14.1/cache.ts":
-          "89eea5f3ce6035a1164b3e655c95f21300498920575ade23161421f5b01967f4",
-        "https://deno.land/x/wasmbuild@0.14.1/loader.ts":
-          "d98d195a715f823151cbc8baa3f32127337628379a02d9eb2a3c5902dbccfc02",
-      },
-    },
-  );
-});
-
-Deno.test("createLockPartForEach", async () => {
-  const updated = await createLockPartForEach(
-    await readLockFile(
-      new URL("../test/fixtures/lockfile/deno.lock", import.meta.url),
-    ),
-  );
-  assertEquals(updated.length, 3);
-  assertObjectMatch(
-    updated[0].data,
-    {
-      packages: {
-        specifiers: {
-          "jsr:@core/match@0.1.x": "jsr:@core/match@0.1.9",
-          "npm:ts-toolbelt@9.6.0": "npm:ts-toolbelt@9.6.0",
-        },
-      },
-    },
-  );
-  assertObjectMatch(
-    updated[1].data,
-    {
-      packages: {
-        specifiers: {
-          "npm:hono@^3": "npm:hono@3.12.12",
-        },
-      },
-    },
-  );
-  assertObjectMatch(
-    updated[2].data,
-    {
-      packages: {
-        specifiers: {
-          "npm:ts-toolbelt@9.6.0": "npm:ts-toolbelt@9.6.0",
-        },
-      },
-    },
-  );
-});
-
-Deno.test("createLockPartForEach - no updates", async () => {
-  const updated = await createLockPartForEach(
-    await readLockFile(
-      new URL("../test/fixtures/lockfile/deno.lock", import.meta.url),
-    ),
-    false,
-  );
-  assertEquals(updated.length, 3);
-  assertObjectMatch(
-    updated[0].data,
-    {
-      version: "3",
-      packages: {
-        specifiers: {
-          "jsr:@core/match@0.1.0": "jsr:@core/match@0.1.0",
-          "npm:ts-toolbelt@9.6.0": "npm:ts-toolbelt@9.6.0",
-        },
-        jsr: { "@core/match@0.1.0": {/* won't check */} },
-        npm: { "ts-toolbelt@9.6.0": {/* won't check */} },
-      },
-    },
-  );
-  assertObjectMatch(
-    updated[1].data,
-    {
-      version: "3",
-      packages: {
-        specifiers: { "npm:hono@3.0.0": "npm:hono@3.0.0" },
-        npm: { "hono@3.0.0": {/* won't check */} },
-      },
-      remote: {},
-    },
-  );
-  assertObjectMatch(
-    updated[2].data,
-    {
-      version: "3",
-      packages: {
-        specifiers: { "npm:ts-toolbelt@9.6.0": "npm:ts-toolbelt@9.6.0" },
-        npm: {
-          "ts-toolbelt@9.6.0": {/* won't check */},
+        jsr: {
+          "@std/testing@0.222.0": {
+            integrity:
+              "a6d10c9fbb1df052ad7f73174d511328c08b7408bdd162ef6c3bc04def49c2ae",
+          },
         },
       },
       remote: {},
-    },
-  );
-});
-
-Deno.test("collectUpdateFromLockFile", async () => {
-  const updates = await collectUpdateFromLockFile(
-    await readLockFile(
-      new URL("../test/fixtures/lockfile/deno.lock", import.meta.url),
-    ),
-  );
-  assertEquals(updates.length, 2);
-  assertObjectMatch(
-    updates[0],
-    {
-      from: {
-        protocol: "jsr:",
-        name: "@core/match",
-        version: "0.1.0",
-        path: "",
-      },
-      to: {
-        protocol: "jsr:",
-        name: "@core/match",
-        version: "0.1.9",
-        path: "",
-      },
-      code: { specifier: "jsr:@core/match@0.1.x", span: undefined },
-      map: undefined,
-    },
-  );
-  assertObjectMatch(
-    updates[1],
-    {
-      from: {
-        protocol: "npm:",
-        name: "hono",
-        version: "3.0.0",
-        path: "",
-      },
-      to: {
-        protocol: "npm:",
-        name: "hono",
-        version: "3.12.12",
-        path: "",
-      },
-      code: { specifier: "npm:hono@^3", span: undefined },
-      map: undefined,
-    },
-  );
-});
-
-Deno.test("collectUpdateFromLockFile - with a patch", async () => {
-  const updates = await collectUpdateFromLockFile(
-    await readLockFile(
-      new URL("../test/fixtures/lockfile/deno.lock", import.meta.url),
-    ),
-    "npm:hono@^3",
-  );
-  assertEquals(updates.length, 1);
-  assertObjectMatch(
-    updates[0],
-    {
-      from: {
-        protocol: "npm:",
-        name: "hono",
-        version: "3.0.0",
-        path: "",
-      },
-      to: {
-        protocol: "npm:",
-        name: "hono",
-        version: "3.12.12",
-        path: "",
-      },
-      code: { specifier: "npm:hono@^3", span: undefined },
-      map: undefined,
-    },
-  );
-});
-
-Deno.test("collectUpdateFromLockFile - extended mapping", async () => {
-  const updates = await collectUpdateFromLockFile(
-    await readLockFile(
-      new URL("../test/fixtures/deno.lock", import.meta.url),
-    ),
-    "jsr:/@std/testing@^0.222.0/bdd",
-  );
-  assertEquals(updates.length, 1);
-  assertObjectMatch(
-    updates[0],
-    {
-      from: {
-        protocol: "jsr:",
-        name: "@std/testing",
-        version: "0.222.0",
-        path: "",
-      },
-      to: {
-        protocol: "jsr:",
-        name: "@std/testing",
-        version: "0.222.1",
-        path: "",
-      },
-      code: { specifier: "jsr:/@std/testing@^0.222.0/bdd", span: undefined },
-    },
-  );
-});
-
-Deno.test("writeToLockFile", async () => {
-  const source = new URL("../test/fixtures/lockfile/mod.ts", import.meta.url);
-  const lockFile = new URL(
-    "../test/fixtures/lockfile/deno.lock",
-    import.meta.url,
-  );
-  const result = await collect(source, {
-    importMap: new URL("deno.json", source),
-    lock: true,
-    lockFile,
+      workspace: { dependencies: ["jsr:@std/testing@^0.222.0"] },
+    });
   });
-  const files = associateByFile(result).filter((file) =>
-    file.kind === "lockfile"
-  ) as FileUpdate<"lockfile">[];
 
-  fs.stub(new URL("../test/fixtures/lockfile", import.meta.url));
-  const actual = await fs.use(async () => {
-    for (const file of files) {
-      await writeToLockfile(file);
-    }
-    return parseLockFileJson(await Deno.readTextFile(lockFile));
-  });
-  const expected = parseLockFileJson(
-    await Deno.readTextFile(
-      new URL("../test/fixtures/lockfile/deno.updated.lock", import.meta.url),
-    ),
-  );
-  // deno-lint-ignore no-explicit-any
-  assertObjectMatch(actual, expected as any);
-});
-
-Deno.test("writeToLockFile", async () => {
-  const source = new URL("../test/fixtures/mod_test.ts", import.meta.url);
-  const lockFile = new URL("deno.lock", source);
-  const result = await collect(source, {
-    importMap: new URL("deno.jsonc", source),
-    lock: true,
-    lockFile,
-  });
-  console.log(result.locks);
-  const files = associateByFile(result).filter((file) =>
-    file.kind === "lockfile"
-  ) as FileUpdate<"lockfile">[];
-  fs.stub(new URL("../test/fixtures", import.meta.url));
-  const actual = await fs.use(async () => {
-    for (const file of files) {
-      await writeToLockfile(file);
-    }
-    return parseLockFileJson(await Deno.readTextFile(lockFile));
+  it("should extract the partial lock along with the dependencies", async () => {
+    const part = await extractJsrPackage("jsr:@std/assert@^0.222.0", lockfile);
+    assertEquals(part.toJson(), {
+      version: "3",
+      packages: {
+        specifiers: {
+          "jsr:@std/assert@^0.222.0": "jsr:@std/assert@0.222.0",
+          "jsr:@std/fmt@^0.222.0": "jsr:@std/fmt@0.222.0",
+        },
+        jsr: {
+          "@std/assert@0.222.0": {
+            "integrity":
+              "cbf00c0d8125a56c087e3d1ea0e638760d47206b30e9d300bad826b811719fc7",
+            "dependencies": [
+              "jsr:@std/fmt@^0.222.0",
+            ],
+          },
+          "@std/fmt@0.222.0": {
+            "integrity":
+              "0eb99babf1cc697d67e76e8753916c037bbc3ce4abcefa321e1465708b0adda1",
+          },
+        },
+      },
+      remote: {},
+      workspace: {
+        dependencies: ["jsr:@std/assert@^0.222.0"],
+      },
+    });
   });
 });
